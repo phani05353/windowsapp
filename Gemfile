5eb37764e809b453f8c1d014623f68f9705b07dd
Phanikumar <phani05353@gmail.com>
2016-04-04 17:18:25 -0400

    1

--- Gemfile
+++ Gemfile
@@ -10,6 +10,10 @@ gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views' 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+
+gem 'haml', '~>4.0.5'
+gem 'simple_form','~>3.0.2'
+gem 'bootstrap-sass','~>3.2.0.2'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   root 'workouts#index'
     
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
       resources :windows
+	  resources :workouts
   # Example resource route with options:
   #   resources :products do
   #     member do
